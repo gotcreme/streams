e4ca2ec2685bfcb15db12fb430598b4b44b15fe8
Volodymyr Dykun <vdy@e-Boks.dk>
2019-09-06 13:29:50 +0300

    implemented crud

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-export default function StreamShow() {
-  return (
-    <div>
-      StreamCreate
-    </div>
-  )
-};
+class StreamShow extends Component {
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+
+	render() {
+		if (!this.props.stream) {
+			return null;
+		}
+
+		const { title, description } = this.props.stream;
+
+		return (
+			<div>
+				<h1>{title}</h1>
+				<h5>{description}</h5>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+			stream: state.streams[ownProps.match.params.id]
+	};
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
